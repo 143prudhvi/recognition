17cef93be281214edd6a442818ac434e661ee9b5
143prudhvi <37447265+143prudhvi@users.noreply.github.com>
2020-10-03 12:42:38 +0530

    Initial commit

---
+++ face/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    # path('', views.index, name='index'),
+    path('face/', views.face, name='face'),
+    path('face/upload', views.detect, name='detect'),
+    path('uploadknown/', views.upload, name='upload'),
+    path('display', views.display, name='display'),
+]
