59fed5b00edefbecd9869f3f256c1c004f04cb7e
143prudhvi <37447265+143prudhvi@users.noreply.github.com>
2020-10-04 17:39:47 +0530

    Re Hashed

--- face/urls.py
+++ face/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('face/', views.face, name='face'),
-    # path('face/upload', views.detect, name='detect'),
+    path('face/upload', views.detect, name='detect'),
     path('', views.upload, name='upload'),
     path('display', views.display, name='display'),
 ]
--- face/views.py
+++ face/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
-# from django.core.files.storage import FileSystemStorage
-# import cv2
-# import os
+from django.core.files.storage import FileSystemStorage
+import cv2
+import os
 from django.core.paginator import Paginator
 import face_recognition
 from .models import Person
 from .form import PersonForm
-# from django.contrib import messages
-# import numpy as np
+from django.contrib import messages
+import numpy as np
 # Create your views here.
 
 
@@ -17,38 +17,38 @@ from .form import PersonForm
 def face(request):
     return render(request, 'face/index.html')
 
-# def detect(request):
-#     myfile = request.FILES['image']
-#     fs = FileSystemStorage()
-#     filename = fs.save('unknown/'+myfile.name, myfile)
-#     uploaded_file_url = fs.url(filename)
-#     encodes = []
-#     names = []
-#     files = []
-#
-#     person = Person.objects.all()
-#     for i in person:
-#         encodes.append(i.name + '_face_encoding')
-#         files.append(i.picture.url[1:])   # /media/known/filename.jpg
-#         names.append(i.name)
-#
-#     for i in range(0, len(encodes)):
-#         image = face_recognition.load_image_file(files[i])
-#         encodes[i] = face_recognition.face_encodings(image)[0]
-#     uploadedimg = cv2.imread(uploaded_file_url[1:])
-#     upload_locations = face_recognition.face_locations(uploadedimg)
-#     upload_encoding = face_recognition.face_encodings(uploadedimg, upload_locations)
-#     for (t, r, b, l), encode in zip(upload_locations, upload_encoding):
-#         result = face_recognition.compare_faces(encodes, encode)
-#         name = 'Unknown'
-#         face_distances = face_recognition.face_distance(encodes, encode)
-#         best_match_index = np.argmin(face_distances)
-#         if result[best_match_index]:
-#             name = names[best_match_index]
-#         cv2.rectangle(uploadedimg, (l, t), (r, b), (0, 255, 255), 3)
-#         cv2.putText(uploadedimg, name, (l+2, t-20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 2)
-#     cv2.imwrite('face/static/output.jpg', uploadedimg)
-#     return render(request, 'face/output.html')
+def detect(request):
+    myfile = request.FILES['image']
+    fs = FileSystemStorage()
+    filename = fs.save('unknown/'+myfile.name, myfile)
+    uploaded_file_url = fs.url(filename)
+    encodes = []
+    names = []
+    files = []
+
+    person = Person.objects.all()
+    for i in person:
+        encodes.append(i.name + '_face_encoding')
+        files.append(i.picture.url[1:])   # /media/known/filename.jpg
+        names.append(i.name)
+
+    for i in range(0, len(encodes)):
+        image = face_recognition.load_image_file(files[i])
+        encodes[i] = face_recognition.face_encodings(image)[0]
+    uploadedimg = cv2.imread(uploaded_file_url[1:])
+    upload_locations = face_recognition.face_locations(uploadedimg)
+    upload_encoding = face_recognition.face_encodings(uploadedimg, upload_locations)
+    for (t, r, b, l), encode in zip(upload_locations, upload_encoding):
+        result = face_recognition.compare_faces(encodes, encode)
+        name = 'Unknown'
+        face_distances = face_recognition.face_distance(encodes, encode)
+        best_match_index = np.argmin(face_distances)
+        if result[best_match_index]:
+            name = names[best_match_index]
+        cv2.rectangle(uploadedimg, (l, t), (r, b), (0, 255, 255), 3)
+        cv2.putText(uploadedimg, name, (l+2, t-20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 2)
+    cv2.imwrite('face/static/output.jpg', uploadedimg)
+    return render(request, 'face/output.html')
 
 def upload(request):
     if request.method == 'POST':
